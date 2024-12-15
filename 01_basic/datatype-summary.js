507132623132272c4b13adfed3024664a723ea60
Somil vaishya <123815919+Somilvaishya@users.noreply.github.com>
2024-12-15 16:52:57 +0000

    read

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,29 @@
+// Primitive Type DataTypes
+
+// Number, String, Boolean, null, undefined, symbol, BigInt
+
+const score = 100;
+const averageSCore = 100.9;
+
+let name = "Rohit sharma";
+
+const isOut = true;
+
+const age = null ;
+
+let innings;
+
+let id = Symbol('123');
+
+
+
+// Non-primitive data type
+
+let teamName = ["ravi","rohit"];
+
+let obj ={
+    age:23,
+    name:"rahul"
+}
+
+
