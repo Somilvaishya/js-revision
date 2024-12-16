c195e8a0911d3361aeb36ac806dd8c58ff5f9c3a
Somil vaishya <123815919+Somilvaishya@users.noreply.github.com>
2024-12-16 18:25:13 +0000

    update

--- 01_basic/05-string.js
+++ 01_basic/05-string.js
@@ -34,4 +34,10 @@ console.log(url.includes('sundar'))
 
 console.log(gameName.split('-'));
 
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
 
